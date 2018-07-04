6d20c7982ab6cd4bbe4ae8d64dedd9e3c5a28a35
GaFree <916353251@qq.com>
2018-07-04 20:36:07 +0800

    new create serializers

--- booktest/urls.py
+++ booktest/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views_old
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
 from . import views
-# router = DefaultRouter()  # 实例化对象
-# router.register(r'books', views_old.BooksViewSet)  # 在路由器中注册类视图
 
+router = DefaultRouter()  # 实例化对象
+# router.register(r'books', views_old.BooksViewSet)  # 在路由器中注册类视图
+# router = SimpleRouter()
+router.register(r'books',views.BookInfoViewSet,base_name='books')
 urlpatterns = [
     # url(r'books/$',views.BooksView)  # 类形式传参
     # 获取全部书籍和增加书籍
@@ -14,9 +16,10 @@ urlpatterns = [
     # # 获取修改删除单个数据
     # url(r'books/(?P<id>\d+)/$', views.BookView.as_view()),
     # 视图集
-    url(r'^books/$', views.BookInfoViewSet.as_view({'get': 'list'})),
-    url(r'^books/(?P<pk>\d+)/$', views.BookInfoViewSet.as_view({'get': 'retrieve'})),
-    url(r'^books/latest/$', views.BookInfoViewSet.as_view({'get': 'latest'})),
-    url(r'^books/read/(?P<pk>\d+)/$', views.BookInfoViewSet.as_view({'post': 'read'})),
+    # url(r'^books/$', views.BookInfoViewSet.as_view({'get': 'list'})),
+    # url(r'^books/(?P<pk>\d+)/$', views.BookInfoViewSet.as_view({'get': 'retrieve'})),
+    # url(r'^books/latest/$', views.BookInfoViewSet.as_view({'get': 'latest'})),
+    # url(r'^books/read/(?P<pk>\d+)/$', views.BookInfoViewSet.as_view({'post': 'read'})),
+    url(r'', include(router.urls))
 ]
 # urlpatterns += router.urls  # 添加路径
